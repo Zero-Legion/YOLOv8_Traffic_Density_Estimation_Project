dd9224fb12ab45dd924f2a70287a72ac7cebc221
ZeroLegion <72653031+Zero-Legion@users.noreply.github.com>
2024-10-24 16:30:21 +0300

    test32

--- detection2.py
+++ detection2.py
@@ -3,11 +3,8 @@ gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GLib
 import os
 import argparse
-import multiprocessing
 import numpy as np
 import setproctitle
-import cv2
-import time
 import hailo
 import supervision as sv
 from hailo_rpi_common import (
@@ -22,35 +19,28 @@ from hailo_rpi_common import (
 # -----------------------------------------------------------------------------------------------
 # User-defined class to be used in the callback function
 # -----------------------------------------------------------------------------------------------
-# Inheritance from the app_callback_class
-class user_app_callback_class(app_callback_class):
+class UserAppCallback(app_callback_class):
     def __init__(self):
         super().__init__()
-        self.new_variable = 42  # New variable example
-    
-    def new_function(self):  # New function example
+        self.new_variable = 42  # Example of a new variable
+
+    def new_function(self):  # Example of a new function
         return "The meaning of life is: "
+
 
 # -----------------------------------------------------------------------------------------------
 # User-defined callback function
 # -----------------------------------------------------------------------------------------------
-
-# This is the callback function that will be called when data is available from the pipeline
 def app_callback(pad, info, user_data):
-    # Get the GstBuffer from the probe info
     buffer = info.get_buffer()
-    # Check if the buffer is valid
     if buffer is None:
         return Gst.PadProbeReturn.OK
 
-    # Get the detections from the buffer
     roi = hailo.get_roi_from_buffer(buffer)
     hailo_detections = roi.get_objects_typed(hailo.HAILO_DETECTION)
     n = len(hailo_detections)
 
-    # Get the caps from the pad
     _, w, h = get_caps_from_pad(pad)
-
 
     boxes = np.zeros((n, 4))
     confidence = np.zeros(n)
@@ -63,16 +53,15 @@ def app_callback(pad, info, user_data):
         tracker_id[i] = detection.get_objects_typed(hailo.HAILO_UNIQUE_ID)[0].get_id()
         bbox = detection.get_bbox()
         boxes[i] = [bbox.xmin() * w, bbox.ymin() * h, bbox.xmax() * w, bbox.ymax() * h]
-    
-    detections = sv.Detections(
-            xyxy=boxes, 
-            confidence=confidence, 
-            class_id=class_id,
-            tracker_id=tracker_id)
 
-    #print(tracker_id, confidence, boxes)
+    detections = sv.Detections(
+        xyxy=boxes, 
+        confidence=confidence, 
+        class_id=class_id,
+        tracker_id=tracker_id)
+
     line_zone.trigger(detections)
-    textoverlay  = app.pipeline.get_by_name("hailo_text")
+    textoverlay = app.pipeline.get_by_name("hailo_text")
     textoverlay.set_property('text', f'OUT: {line_zone.in_count}     |      IN: {line_zone.out_count}')
     textoverlay.set_property('font-desc', 'Sans 36')
 
@@ -80,95 +69,60 @@ def app_callback(pad, info, user_data):
     
 
 # -----------------------------------------------------------------------------------------------
-# User Gstreamer Application
+# User GStreamer Application
 # -----------------------------------------------------------------------------------------------
-
-# This class inherits from the hailo_rpi_common.GStreamerApp class
 class GStreamerDetectionApp(GStreamerApp):
     def __init__(self, args, user_data):
-        # Call the parent class constructor
         super().__init__(args, user_data)
-        # Additional initialization code can be added here
-        # Set Hailo parameters these parameters should be set based on the model used
+
         self.batch_size = 1
         self.network_width = 640
         self.network_height = 640
         self.network_format = "RGB"
-        nms_score_threshold = 0.3 
-        nms_iou_threshold = 0.45
+        self.nms_score_threshold = 0.3 
+        self.nms_iou_threshold = 0.45
         
-        # Temporary code: new postprocess will be merged to TAPPAS.
-        # Check if new postprocess so file exists
-        new_postprocess_path = os.path.join(self.current_path, '/home/naveen/hailo-rpi5-examples/resources/libyolo_hailortpp_post.so')
+        new_postprocess_path = os.path.join(self.current_path, 'resources/libyolo_hailortpp_post.so')
         if os.path.exists(new_postprocess_path):
             self.default_postprocess_so = new_postprocess_path
         else:
             self.default_postprocess_so = os.path.join(self.postprocess_dir, 'libyolo_hailortpp_post.so')
 
-        if args.hef_path is not None:
-            self.hef_path = args.hef_path
-        # Set the HEF file path based on the network
-        elif args.network == "yolov6n":
-            self.hef_path = os.path.join(self.current_path, '../resources/yolov6n.hef')
-        elif args.network == "yolov8s":
-            self.hef_path = os.path.join(self.current_path, '../resources/yolov8s_h8l.hef')
-        elif args.network == "yolox_s_leaky":
-            self.hef_path = os.path.join(self.current_path, '../resources/yolox_s_leaky_h8l_mz.hef')
-        else:
-            assert False, "Invalid network type"
+        self.hef_path = self.get_hef_path(args)
 
-        # User-defined label JSON file
-        if args.labels_json is not None:
-            self.labels_config = f' config-path={args.labels_json} '
-            # Temporary code
-            if not os.path.exists(new_postprocess_path):
-                print("New postprocess so file is missing. It is required to support custom labels. Check documentation for more information.")
-                exit(1)
-        else:
-            self.labels_config = ''
+        self.labels_config = self.get_labels_config(args)
 
-        self.app_callback = app_callback
-    
         self.thresholds_str = (
-            f"nms-score-threshold={nms_score_threshold} "
-            f"nms-iou-threshold={nms_iou_threshold} "
+            f"nms-score-threshold={self.nms_score_threshold} "
+            f"nms-iou-threshold={self.nms_iou_threshold} "
             f"output-format-type=HAILO_FORMAT_TYPE_FLOAT32"
         )
 
-        # Set the process title
         setproctitle.setproctitle("Hailo Detection App")
-
         self.create_pipeline()
 
-    def get_pipeline_string(self):
-        if self.source_type == "rpi":
-            source_element = (
-                #"libcamerasrc name=src_0 auto-focus-mode=AfModeManual ! "
-                "libcamerasrc name=src_0 auto-focus-mode=2 ! "
-                f"video/x-raw, format={self.network_format}, width=1536, height=864  ! "
-                + QUEUE("queue_src_scale")
-                + "videoscale ! "
-                f"video/x-raw, format={self.network_format}, width={self.network_width}, height={self.network_height}, framerate=60/1 ! "
-                #f"video/x-raw, format={self.network_format}, width={self.network_width}, height={self.network_height} ! "
-            )
-        elif self.source_type == "usb":
-            source_element = (
-                f"v4l2src device={self.video_source} name=src_0 ! "
-                "video/x-raw, width=640, height=480, framerate=30/1 ! "
-            )
-        else:
-            source_element = (
-                f"filesrc location={self.video_source} name=src_0 ! "
-                + QUEUE("queue_dec264")
-                + " qtdemux ! h264parse ! avdec_h264 max-threads=2 ! "
-                " video/x-raw, format=I420 ! "
-            )
-        source_element += QUEUE("queue_scale")
-        source_element += "videoscale n-threads=2 ! "
-        source_element += QUEUE("queue_src_convert")
-        source_element += "videoconvert n-threads=3 name=src_convert qos=false ! "
-        source_element += f"video/x-raw, format={self.network_format}, width={self.network_width}, height={self.network_height}, pixel-aspect-ratio=1/1 ! "
+    def get_hef_path(self, args):
+        if args.hef_path:
+            return args.hef_path
+        
+        hef_paths = {
+            "yolov6n": os.path.join(self.current_path, '../resources/yolov6n.hef'),
+            "yolov8s": os.path.join(self.current_path, '../resources/yolov8s_h8l.hef'),
+            "yolox_s_leaky": os.path.join(self.current_path, '../resources/yolox_s_leaky_h8l_mz.hef'),
+        }
+        
+        return hef_paths.get(args.network, None)
 
+    def get_labels_config(self, args):
+        if args.labels_json:
+            if not os.path.exists(new_postprocess_path):
+                print("New postprocess so file is missing. It is required to support custom labels. Check documentation for more information.")
+                exit(1)
+            return f' config-path={args.labels_json} '
+        return ''
+
+    def get_pipeline_string(self):
+        source_element = self.get_source_element()
         pipeline_string = (
             "hailomuxer name=hmux "
             + source_element
@@ -201,9 +155,30 @@ class GStreamerDetectionApp(GStreamerApp):
         print(pipeline_string)
         return pipeline_string
 
+    def get_source_element(self):
+        if self.source_type == "rpi":
+            return (
+                "libcamerasrc name=src_0 auto-focus-mode=2 ! "
+                f"video/x-raw, format={self.network_format}, width=1536, height=864 ! "
+                + QUEUE("queue_src_scale")
+                + "videoscale ! "
+                f"video/x-raw, format={self.network_format}, width={self.network_width}, height={self.network_height}, framerate=60/1 ! "
+            )
+        elif self.source_type == "usb":
+            return (
+                f"v4l2src device={self.video_source} name=src_0 ! "
+                "video/x-raw, width=640, height=480, framerate=30/1 ! "
+            )
+        else:  # Assuming 'file'
+            return (
+                f"filesrc location={self.video_source} name=src_0 ! "
+                + QUEUE("queue_dec264")
+                + "qtdemux ! h264parse ! avdec_h264 max-threads=2 ! "
+                "video/x-raw, format=I420 ! "
+            )
+
 if __name__ == "__main__":
-    # Create an instance of the user app callback class
-    user_data = user_app_callback_class()
+    user_data = UserAppCallback()
 
     START = sv.Point(0, 340)
     END = sv.Point(640, 340)
@@ -211,7 +186,6 @@ if __name__ == "__main__":
     line_zone = sv.LineZone(start=START, end=END, triggering_anchors=(sv.Position.BOTTOM_LEFT, sv.Position.BOTTOM_RIGHT))
 
     parser = get_default_parser()
-    # Add additional arguments here
     parser.add_argument(
         "--network",
         default="yolov6n",
@@ -226,7 +200,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--labels-json",
         default=None,
-        help="Path to costume labels JSON file",
+        help="Path to custom labels JSON file",
     )
     args = parser.parse_args()
     app = GStreamerDetectionApp(args, user_data)
